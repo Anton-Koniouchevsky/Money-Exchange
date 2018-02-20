e50e0e159956a0a8dd3d87ba67d543745cfe7c60
Anton-Koniouchevsky <anton.koniouchevsky@gmail.com>
2018-02-20 10:54:10 +0300

    Add money-exchange implementation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
-// PLEASE DON'T change function name
+const coins = {
+    'H': 50,
+    'Q': 25,
+    'D': 10,
+    'N': 5,
+    'P': 1,
+}
+
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    if (currency > 10000) return { error: "You are rich, my friend! We don't have so much coins for exchange" }
+    return Object.keys(coins).reduce((result, current) => {
+        if (currency >= coins[current]) {
+            let numberOfCoins = Math.floor(currency / coins[current]);
+            currency -= numberOfCoins * coins[current];
+            result[current] = numberOfCoins;
+        }
+        return result;
+    }, {});
 }
